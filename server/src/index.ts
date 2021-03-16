8137dba57a658b18ab6ed06ed68e544bc50b52f7
TwitoCode <toheebeji@gmail.com>
2021-03-16 13:09:56 -0400

    Added a root resolver, working on server startup

---
+++ server/src/index.ts
@@ -0,0 +1,62 @@
+import { ApolloServer } from "apollo-server-express";
+import connectMongo from "connect-mongodb-session";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import session from "express-session";
+import mongoose from "mongoose";
+import passport from "passport";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { Container } from "typedi";
+
+const main = async () => {
+	const app = express();
+	const PORT = process.env.PORT || 5000;
+
+	const schema = await buildSchema({ resolvers: [__dirname + "/resolvers/*.ts"], container: Container });
+
+	const server = new ApolloServer({
+		schema,
+	});
+
+	try {
+		await mongoose.connect(process.env.MONGO || "", {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	app.use(cookieParser());
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+
+	const timeTillExpire = 1000 * 60 * 60 * 24;
+	const Store = connectMongo(session);
+
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET || "test",
+			saveUninitialized: false,
+			resave: false,
+			cookie: {
+				maxAge: timeTillExpire,
+			},
+			store: new Store({
+				collection: "chat-site-sessions",
+				uri: process.env.DB_URL!,
+				expires: timeTillExpire,
+			}),
+		})
+	);
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+	server.applyMiddleware({ app });
+	app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+};
+
+main();
